cf5112d72f033eac3c84acc3aa8ce082293d3de5
sipaz00 <sarairiartepaz@gmail.com>
2025-02-16 16:04:08 +0100

    4

--- RE.sql
+++ RE.sql
@@ -1,5 +1,68 @@
 USE final_project;
 
-SELECT 
+-- total number of countries
+SELECT COUNT(DISTINCT country) AS total_countries
+FROM country_df;
+
+-- types of technology
+SELECT DISTINCT technology
+FROM country_df;
+
+SELECT DISTINCT technology
+FROM region_df;
+
+-- overview of the full tables
+SELECT*
 FROM region_df
-GROUP BY region, indicator;
+LIMIT 10;
+
+SELECT*
+FROM country_df
+LIMIT 10;
+
+-- total annual investment by continent, filtered by year
+SELECT continent, SUM(`public_investments_(2021_million_usd)`) AS total_annual_investment
+FROM country_df
+WHERE year = 2022  -- adjust accordingly
+GROUP BY continent
+ORDER BY total_annual_investment DESC;
+
+-- average investment per country and technology, filter by year
+SELECT country, technology, AVG(`public_investments_(2021_million_usd)`) AS avg_investment
+FROM country_df
+WHERE year = 2022  -- year filter
+GROUP BY country, technology
+ORDER BY avg_investment DESC
+LIMIT 10;
+
+-- total annual investment on the different technologies, filter by year
+SELECT technology, SUM(`public_investments_(2021_million_usd)`) AS total_investment
+FROM country_df
+WHERE year = 2021 -- year filter 
+GROUP BY technology
+ORDER BY total_investment DESC
+LIMIT 10;
+
+-- annual average investment per country and tech
+SELECT year, country, technology, AVG(`public_investments_(2021_million_usd)`) AS avg_annual_investment
+FROM country_df
+GROUP BY year, country, technology
+ORDER BY avg_annual_investment DESC
+LIMIT 10;
+
+-- Total RE share of electricity capacity (%) per region, filter by year
+SELECT region, SUM(`re_share_of_electricity_capacity_and_generation_%`) AS total_re_capacity
+FROM region_df
+WHERE year = 2022
+AND indicator = 'RE share of electricity capacity (%)' -- year filter 
+GROUP BY region
+ORDER BY total_re_capacity DESC;
+
+-- Total RE share of electricity generation (%) per region, filter by year
+SELECT region, SUM(`re_share_of_electricity_capacity_and_generation_%`) AS total_re_generation
+FROM region_df
+WHERE year = 2022 AND indicator = 'RE share of electricity generation (%)' -- year filter 
+GROUP BY region
+ORDER BY total_re_generation DESC;
+
+
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# project_final
+# Renewable Energy Analysis: Solar Energy🔆
+
+# 🔋 Overview
+This project analyses renewable energy trends, using data from the International Renewable Energy Agency (IRENA). It focuses on solar energy share in electricity and capacity generation, as well as public investments across different regions, continents and countries. 
+- Data source: [International Renewable Energy Agency (IRENA)](https://www.irena.org/Data). 
+
+# 📚 Libraries
+- **pandas**: data maniputaltion and analysis
+- **numpy**: for numerical computing
+- **matplotlib.pyplot**: visualization tool
+- **seaborn**: statistical visualization library builtt on Matplotlib
+- **scipy.stats**: statitical functions for hypothesis testing and probability distributions
+
+
+# 📊 Visualization
+**Tableau**:
+- 💰 [Public Investment Evolution](https://public.tableau.com/app/profile/sara.iriarte/viz/SolarEnergyAnalysisPublicInvestment/PublicInvestment?publish=yes)
+
+- 📉 [Electricity statistics Evolution](https://public.tableau.com/app/profile/sara.iriarte/viz/SolarEnergyAnalysisElectricityStatistics/ElectricityStats?publish=yes)
+
+**Presentation**: [Google Slides](https://docs.google.com/presentation/d/12NR6QySms6yp8bdLKZU0xoK15xmZud5ab3oYGnoj-vw/edit?usp=sharing)
